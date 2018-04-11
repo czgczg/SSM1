2d7a2f82557c8a0670a34189ea7705263a738ec5
fdsa <601698747@qq.com>
2018-04-11 11:02:26 +0800

    Passenger add gender下拉框示例

--- src/com/cskaoyan/controller/PassengerController.java
+++ src/com/cskaoyan/controller/PassengerController.java
@@ -3,11 +3,35 @@ package com.cskaoyan.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 @Controller
 public class PassengerController {
     @RequestMapping("Passenger/tolist")
     public String roomsetToList(){
 
         return "/WEB-INF/jsp/passenger/list.jsp";
+    }
+
+
+    //
+
+    @RequestMapping("Passenger/toadd")
+    public String passengerToAdd(HttpServletRequest request){
+        HashMap<String,String> male = new HashMap<>();
+        male.put("far_id","1");
+        male.put("attributeDetailsName","男");
+        HashMap<String,String> female = new HashMap<>();
+        female.put("far_id","2");
+        female.put("attributeDetailsName","女");
+        ArrayList<HashMap> listGender = new ArrayList<>();
+        listGender.add(male);
+        listGender.add(female);
+        request.setAttribute("listGender",listGender);
+
+
+        return "/WEB-INF/jsp/passenger/add.jsp";
     }
 }
