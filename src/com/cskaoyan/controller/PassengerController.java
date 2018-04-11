a94699129f1ecdb5afcadae15ab7984411aef5bf
jingsmith <goog1e491488323@gmail.com>
2018-04-11 18:08:46 +0800

    修复一些小bug

--- src/com/cskaoyan/controller/PassengerController.java
+++ src/com/cskaoyan/controller/PassengerController.java
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-@RequestMapping("/passenger")
+@RequestMapping("/Passenger")
 @Controller
 public class PassengerController {
 
