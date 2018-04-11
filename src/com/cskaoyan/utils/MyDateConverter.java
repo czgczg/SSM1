7a00d72a457dc0df3e7fc036f9df2d1c16aa52a1
fdsa <601698747@qq.com>
2018-04-11 13:56:18 +0800

    新增时间工具类以及配置

---
+++ src/com/cskaoyan/utils/MyDateConverter.java
@@ -0,0 +1,21 @@
+package com.cskaoyan.utils;
+
+import org.springframework.core.convert.converter.Converter;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyDateConverter implements Converter<String,Date> {
+    @Override
+    public Date convert(String s) {
+        Date parse = null;
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            parse = simpleDateFormat.parse(s);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return parse;
+    }
+}
