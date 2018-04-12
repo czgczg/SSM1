e1e6242f4c9d30e3149eeb4b698456197441b70b
fdsa <601698747@qq.com>
2018-04-12 23:47:09 +0800

    新增预定222

--- src/com/cskaoyan/controller/PredetermineController.java
+++ src/com/cskaoyan/controller/PredetermineController.java
@@ -4,6 +4,7 @@ import com.cskaoyan.bean.Ordermain;
 import com.cskaoyan.bean.Orderpayment;
 import com.cskaoyan.bean.Passenger;
 import com.cskaoyan.bean.Roomset;
+import com.cskaoyan.dao.OrdermainMapper;
 import com.cskaoyan.dao.OrderpaymentMapper;
 import com.cskaoyan.dao.PassengerMapper;
 import com.cskaoyan.dao.RoomsetMapper;
@@ -15,8 +16,12 @@ import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("/Predetermine")
 @Controller
@@ -24,6 +29,7 @@ public class PredetermineController {
 
     @GetMapping("/tolist")
     public String roomsetToList(){
+
 
         return "/WEB-INF/jsp/predetermine/list.jsp";
     }
@@ -36,8 +42,8 @@ public class PredetermineController {
 
     @GetMapping("/toadd")
     public String predetermineToAdd(HttpServletRequest request){
-        String id = request.getParameter("id");
-        String name = request.getParameter("name");
+        String id = request.getParameter("id");  //乘客id
+        String name = request.getParameter("name");   //
         String type = request.getParameter("type");  //1为团队 2为散客
 
         request.setAttribute("name",name);
@@ -63,17 +69,27 @@ public class PredetermineController {
     public List<Roomset> predetermineSelectRoom(Roomset roomset){
         //System.out.println(roomset.getRoomNumber());
         List<Roomset> roomsets = roomsetMapper.selectAllRoomset();
-        System.out.println(roomsets);
+        //System.out.println(roomsets);
         return roomsets;
     }
 
+    //selectTarget
+    @PostMapping("/selectPassenger")
+    @ResponseBody
+    public List<Passenger> predetermineSelectTarget(Roomset roomset){
 
+        List<Passenger> allPassenger = passengerMapper.findAllPassenger();
+        System.out.println(allPassenger);
+        return allPassenger;
+    }
+
+    @Autowired
+    OrdermainMapper ordermainMapper;
 
     //http://localhost:8080/Predetermine/add.do?id=2&type=1&roomIdShuZu=undefined
 
     //新增订单
     @PostMapping("/add")
-
 
 
 
@@ -87,25 +103,54 @@ public class PredetermineController {
      }
      */
 
-    public String addOrder(Ordermain ordermain, HttpServletRequest request){
+    @ResponseBody
+    public Ordermain addOrder(Ordermain ordermain, HttpServletRequest request,Date arriveTime ){
         String id = request.getParameter("id");
-        Roomset roomset = roomsetMapper.selectByPrimaryKey(Integer.valueOf(id));
+        ordermain.setPassengerOrReceiveID(Integer.valueOf(id));
 
         String type = request.getParameter("type");//1是团队2是散客
-        if(type.equals(1)){
+        ordermain.setReceiveTargetID(Integer.valueOf(type));
+        //订单号201804120001
+        String ordId = UUID.randomUUID().toString();
+        ordermain.setOrdID(ordId);
+        if(type.equals("56")){
             //取团队对象
-
-        }else if(type.equals(2)){
+            ordermain.setTeamname(ordermain.getCommodityName());
+        }else if(type.equals("55")){
             //取散客对象
+            ordermain.setName(ordermain.getCommodityName());
         }
+
+
+        //状态设置为未安排66
+        ordermain.setState(66);
+
+        //改房间真实状态
+        //roomsetMapper.
+
+
+        if(null == ordermain.getRentOutTypeName()){
+            ordermain.setRentOutTypeName(1);
+        }
+
+
+        ordermain.setPayWayName(orderpaymentMapper.selectByPrimaryKey(ordermain.getPayWayID()).getAttributeDetailsName());
+
         String[] split = request.getParameter("roomIdShuZu").split(",");
         for (String s : split) {
+//            System.out.println(s);//1234
+            int i = Integer.parseInt(s);
+            System.out.println(i);
+            //Roomset roomset = roomsetMapper.selectByPrimaryKey(i);
+            //ordermain.setRoomNumber(roomset.getRoomNumber());
+            //ordermain.setGuestRoomLevelName(roomset.getGuestRoomLevelName());
 
         }
 
         System.out.println(ordermain);
+        ordermainMapper.insert(ordermain);
 
-        return "";
+        return ordermain;
     }
 
 
