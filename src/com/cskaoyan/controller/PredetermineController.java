383fb6a2d4ce88be6cca997d32bd623bb0f5088d
CarryWill <wujiangfan14@hotmail.com>
2018-04-12 16:33:47 +0800

    添加一点与前端交互的空方法，返回类型可以自己修改

--- src/com/cskaoyan/controller/PredetermineController.java
+++ src/com/cskaoyan/controller/PredetermineController.java
@@ -1,16 +1,44 @@
 package com.cskaoyan.controller;
 
+import com.cskaoyan.bean.Orderpayment;
+import com.cskaoyan.bean.Passenger;
+import com.cskaoyan.bean.Roomset;
+import com.cskaoyan.dao.OrderpaymentMapper;
+import com.cskaoyan.dao.PassengerMapper;
+import com.cskaoyan.dao.RoomsetMapper;
+import org.apache.tools.ant.taskdefs.condition.Http;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 
 @RequestMapping("/Predetermine")
 @Controller
 public class PredetermineController {
 
+
+    @Autowired
+    PassengerMapper passengerMapper;
+    @Autowired
+    OrderpaymentMapper orderpaymentMapper;
+    @Autowired
+    RoomsetMapper roomsetMapper;
+
+
+
+
     @GetMapping("/tolist")
-    public String roomsetToList(){
+    public String roomsetToList(HttpServletRequest request){
+        String state = request.getParameter("state");//66未安排，67已安排
 
         return "/WEB-INF/jsp/predetermine/list.jsp";
     }
@@ -20,4 +48,46 @@ public class PredetermineController {
 
     }
 
+
+    @RequestMapping("/selectTarget")
+    public void selectTarget(){
+
+    }
+
+
+
+    @GetMapping("/toadd")
+    public String predetermineToAdd(HttpServletRequest request){
+        String id = request.getParameter("id");
+        String name = request.getParameter("name");
+        String type = request.getParameter("type");  //1为团队 2为散客
+
+        request.setAttribute("name",name);
+        request.setAttribute("id",id);
+        request.setAttribute("type",type);
+
+        ArrayList<Orderpayment> allOrderPayment = orderpaymentMapper.getAllOrderPayment();
+
+        request.setAttribute("listOne",allOrderPayment);
+        Passenger passenger = passengerMapper.selectByPrimaryKey(Integer.valueOf(id));
+
+
+        return "/WEB-INF/jsp/predetermine/add.jsp";
+    }
+
+
+    //根据room num查询
+    @PostMapping("/selectRoom")
+    @ResponseBody
+    public List<Roomset> predetermineSelectRoom(Roomset roomset){
+        //System.out.println(roomset.getRoomNumber());
+        List<Roomset> roomsets = roomsetMapper.selectAllRoomset();
+        System.out.println(roomsets);
+        return roomsets;
+    }
+
+@RequestMapping("/comfirmPassenger")
+    public void comfirmPassenger(){
+
+}
 }
--- src/com/cskaoyan/controller/StayRegisterController.java
+++ src/com/cskaoyan/controller/StayRegisterController.java
@@ -2,15 +2,168 @@ package com.cskaoyan.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequestMapping("/StayRegister")
 @Controller
 public class StayRegisterController {
 
-    @GetMapping("/tolist")
-    public String roomsetToList(){
 
+    /*
+    * 显示页面
+    * */
+    @GetMapping("/tolist")
+    public String roomsetToList(HttpServletRequest request){
+        String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");//55旅客，56团队
+        String isBillID = request.getParameter("isBillID");//68未结账，69已结账
         return "/WEB-INF/jsp/stayregister/list.jsp";
     }
+/*
+* 登记
+* */
+    @RequestMapping("/toregister")
+    public void toregister(HttpServletRequest request){
+        String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");//55旅客，56团队
+        String id = request.getParameter("id");
+        String roomNumber = request.getParameter("roomNumber");
+
+    }
+
+
+
+//选择旅客
+
+    @PostMapping("/selectPassenger")
+    public void selectPassenger(){
+
+    }
+
+
+//确认旅客
+
+    @RequestMapping("/comfirmPassenger")
+    public String comfirmPassenger(){
+        return "redirect:tolist.do";
+    }
+/*
+*安排房间
+ *  */
+    @RequestMapping("/toanrrangeroom")
+    public void toanrrangeroom(){
+
+    }
+/*
+* 换房
+* */
+@RequestMapping("/tochangeroom")
+public void tochangeroom(HttpServletRequest request){
+    String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");//55旅客，56团队
+    String id = request.getParameter("id");
+    String lvKeName = request.getParameter("lvKeName");
+
+}
+//选择房间
+    @RequestMapping("/changeRoomSelectPassenger")
+    public void changeRoomSelectPassenger(){
+
+
+
+    }
+
+
+
+    /*
+    * 押金记录
+    * */
+    @RequestMapping("/todeposit")
+    public void todeposit(HttpServletRequest request){
+        String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");
+        String lvKeName = request.getParameter("lvKeName");
+        String id = request.getParameter("id");
+
+    }
+
+
+    /*
+    * 旅客消费
+    * */
+    @RequestMapping("/toconsumption")
+    public void toconsumption(HttpServletRequest request){
+        String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");
+        String lvKeName = request.getParameter("lvKeName");
+        String id = request.getParameter("id");
+        String roomNumber = request.getParameter("roomNumber");
+        String isBillID = request.getParameter("isBillID");//68未结账，69已结账
+        String number = request.getParameter("Number");
+        request.getParameter("consumptionStayRegisterID");
+
+
+    }
+//添加商品
+@RequestMapping("/tianJiaShangPin")
+    public void tianJiaShangPin(){
+
+}
+//选择商品
+@RequestMapping("/consumption")
+    public void consumption(HttpServletRequest request){
+    String id = request.getParameter("id");
+    String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");
+    String number = request.getParameter("Number");
+    String consumptionStayRegisterID = request.getParameter("consumptionStayRegisterID");
+
+}
+//删除商品
+@RequestMapping("/consumptionDelete")
+    public void consumptionDelete(HttpServletRequest request){
+    String id = request.getParameter("id");
+    String consumptionStayRegisterID = request.getParameter("consumptionStayRegisterID");
+}
+
+/*
+* 转换
+* */
+//转入团队房间信息
+        @RequestMapping("/toshiftteam")
+        public void toshiftteam(HttpServletRequest request){
+
+        }
+
+//转为散客
+    @RequestMapping("/toshiftpersonage")
+        public void toshiftpersonage(HttpServletRequest request){
+        String id = request.getParameter("id");
+        String stayregisterdetailsId = request.getParameter("stayregisterdetailsId");
+    }
+    //转入
+    @RequestMapping("/changeOver")
+    public String changeOver(HttpServletRequest request){
+        String id = request.getParameter("id");
+        String lvKeLeiXingId = request.getParameter("LvKeLeiXingId");
+        return "redirect:tolist.do";
+    }
+
+
+
+/*
+* 结账
+* */
+        @RequestMapping("/topay")
+    public void topay(HttpServletRequest request){
+            String id = request.getParameter("id");
+        }
+
+        @RequestMapping("pay")
+public String pay(HttpServletRequest request){
+            String id = request.getParameter("id");
+            String payTime = request.getParameter("payTime");
+            String payWay = request.getParameter("payWay");
+            String roomId = request.getParameter("roomId");
+            return "redirect:tolist.do";
+        }
+
+
 }
