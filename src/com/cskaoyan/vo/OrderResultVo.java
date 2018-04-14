2e114da817bb4a748c5d21f6ec72fc9f685bcd49
fdsa <601698747@qq.com>
2018-04-14 11:46:03 +0800

    预定页面补充提交

--- src/com/cskaoyan/vo/OrderResultVo.java
+++ src/com/cskaoyan/vo/OrderResultVo.java
@@ -1,4 +1,15 @@
 package com.cskaoyan.vo;
 
-public class OrderResultVo {
+import com.cskaoyan.bean.Ordermain;
+
+import java.util.ArrayList;
+
+public class OrderResultVo extends Ordermain{
+    //result包装类
+    //电话号码
+    private String passengerContactPhoneNumber;
+
+    //
+    private int remind;
+
 }
