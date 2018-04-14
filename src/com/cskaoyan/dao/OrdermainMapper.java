06476fadb50c7992c9416e995441ab8ceb2ddf65
fdsa <601698747@qq.com>
2018-04-14 11:44:31 +0800

    金兰瑛：4-14-01预定列表页面相关代码提交

--- src/com/cskaoyan/dao/OrdermainMapper.java
+++ src/com/cskaoyan/dao/OrdermainMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface OrdermainMapper {
@@ -46,8 +47,23 @@ public interface OrdermainMapper {
      *
      * @mbg.generated Thu Apr 12 17:56:44 CST 2018
      */
-    int updateByPrimaryKey(Ordermain record);
 
+    //安排房间
+    Boolean pushRoomset(String ordId);
+
+    //查询所有未安排的订单
+    ArrayList<Ordermain> getAllOrderIsBookingOn();
+
+    //查询所有已安排安排的订单
+    ArrayList<Ordermain> getAllOrderAlreadyBooking();
+
+    //根据状态查询总数
+    @Select("select count(1) from ordermain where receiveTargetID in (#{receiveTargetID}) and del_flag=0")
+    Integer getPageOfOrdermains(String receiveTargetID);
+
+    //查询部分订单
+    @Select("SELECT * FROM ordermain WHERE del_flag=0 AND receiveTargetID=#{receiveTargetID} AND ( NAME LIKE #{name} OR teamName LIKE  #{teamname} )  LIMIT #{offset},#{limit}")
+    List<Ordermain> findPartOrdermains(HashMap<String, Object> hashMap);
     List<Ordermain> selectAllOrderIsBeBooking();
 
     /**
